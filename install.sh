e7072faf79892a6807111999194f237d6b27a3da
Kent <kentjhall3@hotmail.com>
2020-01-23 15:28:18 -0500

    tweak

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
 	brew install macvim cmake
 	brew upgrade macvim cmake
 else
-	apt install vim cmake
+	sudo apt install vim cmake
 fi
 cd $HOME/.vim/plugged/YouCompleteMe
 git submodule update --init --recursive
