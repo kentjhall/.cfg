043a0ce8f94e1ba2da897f507443587cd9b6dd64
Kent <kentjhall3@hotmail.com>
2020-05-15 10:16:44 -0400

    YCM install fixes

--- install.sh
+++ install.sh
@@ -3,34 +3,37 @@
 # copy dotfiles to home directory
 cd $HOME/.cfg
 for cfg in $(ls -A); do
-	if [[ $cfg != ".git" ]] && [[ $cfg != "install.sh" ]]; then cp -R $cfg $HOME; fi
+	if [ "$cfg" != ".git" ] && [ "$cfg" != "install.sh" ]; then cp -R $cfg $HOME; fi
 done
 
 # source bashrc
 source $HOME/.bashrc
 
-# check if YCM is already installed
+# determine whether to go through YCM install process
 YCM_PATH="$HOME/.vim/plugged/YouCompleteMe"
-ycm_installed=true
-if [ ! -d $YCM_PATH ]; then
-	ycm_installed=false
-fi
+[ ! -d "$YCM_PATH" ] && YCM_INSTALL=true || YCM_INSTALL=false
 
 # install vim plugins
 vim +PlugInstall +qall 
 
-# install YMC vim plugin if not already installed
-if [ $ycm_installed != "true" ]; then
-	if [[ "$OSTYPE" == "darwin"* ]]; then
-		if ! type "brew" > /dev/null; then
-			/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-		fi		
-		brew install macvim cmake
-		brew upgrade macvim cmake
-	else
-		sudo apt install vim-gtk clang cmake curl python python-dev
+# set OS-specific variables
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	# on macOS, make sure homebrew is installed
+	if ! type "brew" > /dev/null; then
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
-	cd $YCM_PATH
+	PM="brew"
+	YCM_DEPS="cmake macvim python mono go nodejs"
+else
+	PM="sudo apt-get"
+	YCM_DEPS="build-essential cmake vim python3-dev mono-devel golang-go nodejs"
+fi	
+
+# install YMC vim plugin if not already installed
+if $YCM_INSTALL; then
+	$PM install $YCM_DEPS
+	$PM upgrade $YCM_DEPS
+	cd "$YCM_PATH"
 	git submodule update --init --recursive
-	./install.py --clang-completer --system-libclang
+	python3 install.py --all
 fi
