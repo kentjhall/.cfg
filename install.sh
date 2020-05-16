c71968c2abfd56eaba1393b601a2c9a3a07adc51
Kent Hall <kjh2166@columbia.edu>
2020-05-15 21:10:58 -0400

    install.sh tweaks

--- install.sh
+++ install.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# parse arguments
+DO_ALL=false
+for arg in "$@"; do
+	[ "$arg" == "--all" ] && DO_ALL=true
+done
+
 # copy dotfiles to home directory
 cd $HOME/.cfg
 for cfg in $(ls -A); do
 	if [ "$cfg" != ".git" ] && [ "$cfg" != "install.sh" ]; then cp -R $cfg $HOME; fi
 done
-
-# source bashrc
-source $HOME/.bashrc
 
 # determine whether to go through YCM install process
 YCM_PATH="$HOME/.vim/plugged/YouCompleteMe"
@@ -30,9 +33,9 @@ fi
 vim +PlugInstall +qall 
 
 # install YMC vim plugin if not already installed
-if $YCM_INSTALL; then
-	$PM install $YCM_DEPS
-	$PM upgrade $YCM_DEPS
+if $YCM_INSTALL || $DO_ALL; then
+	yes | $PM install $YCM_DEPS
+	yes | $PM upgrade $YCM_DEPS
 	cd "$YCM_PATH"
 	git submodule update --init --recursive
 	python3 install.py --all
