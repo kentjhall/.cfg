c6c0f9f68ca69fc32b62c3a186c2a5542d6cfc7e
Kent <kentjhall3@hotmail.com>
2020-05-16 11:34:44 -0400

    hopefully fixed ls issue once and for all (uses the colors flag on linux, doesn't on mac); also, modified sourceem.sh to work regardless of where .cfg is located or where it is sourced from

--- sourceme.sh
+++ sourceme.sh
@@ -1,12 +1,35 @@
+# set OS-specific variables
+if [[ "$OSTYPE" == "darwin"* ]]; then
+	LS_OPTS="-A"
+	PM="brew"
+	YCM_DEPS="cmake macvim python mono go nodejs"
+
+	# on macOS, make sure homebrew is installed
+	if ! type "brew" > /dev/null; then
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	fi
+else
+	LS_OPTS="-A --color=never"
+	PM="sudo apt-get"
+	YCM_DEPS="build-essential cmake vim-gtk python3-dev mono-devel golang-go nodejs"
+
+	sudo ln -sf /usr/bin/python3 /usr/local/bin/python3 # an annoying fix for Linux/Mac compat on the YCM setup
+fi	
+
 # parse arguments
-DO_ALL=false
+FULL_INSTALL=false
 for arg in "$@"; do
-	[ "$arg" == "--all" ] && DO_ALL=true
+	case "$arg" in
+		--full)
+			FULL_INSTALL=true
+			;;
+	esac
 done
 
 # copy dotfiles to home directory
-cd "$HOME/.cfg"
-for cfg in $(ls -A --color=never); do
+START_PWD="$PWD"
+cd "$(dirname $BASH_SOURCE)" 
+for cfg in $(ls $LS_OPTS); do
 	case "$cfg" in
 		.git | sourceme.sh | host-specific)
 			;;
@@ -16,35 +39,19 @@ for cfg in $(ls -A --color=never); do
 	esac
 done
 
-# copy any host-specific dotfiles
+# copy any machine-specific dotfiles
 HSP_PATH="host-specific/$HOSTNAME"
-[ -d "$HSP_PATH" ] && for cfg in $(ls -A --color=never "$HSP_PATH"); do cp -R "$HSP_PATH/$cfg" "$HOME"; done
+[ -d "$HSP_PATH" ] && for cfg in $(ls $LS_OPTS "$HSP_PATH"); do cp -R "$HSP_PATH/$cfg" "$HOME"; done
 
 # source the copied bashrc
 . "$HOME/.bashrc"
 
-# set OS-specific variables
-if [[ "$OSTYPE" == "darwin"* ]]; then
-	PM="brew"
-	YCM_DEPS="cmake macvim python mono go nodejs"
-
-	# on macOS, make sure homebrew is installed
-	if ! type "brew" > /dev/null; then
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	fi
-else
-	PM="sudo apt-get"
-	YCM_DEPS="build-essential cmake vim-gtk python3-dev mono-devel golang-go nodejs"
-
-	sudo ln -sf /usr/bin/python3 /usr/local/bin/python3 # an annoying fix for Linux/Mac compat on the YCM setup
-fi	
-
 # determine whether to go through YCM install process
 YCM_PATH="$HOME/.vim/plugged/YouCompleteMe"
-[ ! -d "$YCM_PATH" ] && YCM_INSTALL=true || YCM_INSTALL=false
+{ [ ! -d "$YCM_PATH" ] || $FULL_INSTALL; } && YCM_INSTALL=true || YCM_INSTALL=false
 
 # YCM pre-install
-if $YCM_INSTALL || $DO_ALL; then
+if $YCM_INSTALL; then
 	yes | $PM install $YCM_DEPS
 	yes | $PM upgrade $YCM_DEPS
 fi
@@ -53,8 +60,11 @@ fi
 vim +PlugInstall +qall 
 
 # YCM full install
-if $YCM_INSTALL || $DO_ALL; then
+if $YCM_INSTALL; then
 	cd "$YCM_PATH"
 	git submodule update --init --recursive
 	python3 install.py --all
 fi
+
+# return to starting location
+cd "$START_PWD"
