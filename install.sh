8ea2fe4c1935ed8e3da9baf1a80315911c8d5994
Kent Hall <kentjhall3@hotmail.com>
2020-05-16 09:32:59 -0400

    install.sh installs proper vim on linux now

--- install.sh
+++ install.sh
@@ -18,25 +18,31 @@ YCM_PATH="$HOME/.vim/plugged/YouCompleteMe"
 
 # set OS-specific variables
 if [[ "$OSTYPE" == "darwin"* ]]; then
+	PM="brew"
+	YCM_DEPS="cmake macvim python mono go nodejs"
+
 	# on macOS, make sure homebrew is installed
 	if ! type "brew" > /dev/null; then
 		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
-	PM="brew"
-	YCM_DEPS="cmake macvim python mono go nodejs"
 else
-	sudo ln -sf /usr/bin/python3 /usr/local/bin/python3 # an annoying fix for YCM Linux/Mac compat
 	PM="sudo apt-get"
-	YCM_DEPS="build-essential cmake vim python3-dev mono-devel golang-go nodejs"
+	YCM_DEPS="build-essential cmake vim-gtk python3-dev mono-devel golang-go nodejs"
+
+	sudo ln -sf /usr/bin/python3 /usr/local/bin/python3 # an annoying fix for Linux/Mac compat on the YCM setup
 fi	
+
+# YCM pre-install
+if $YCM_INSTALL || $DO_ALL; then
+	yes | $PM install $YCM_DEPS
+	yes | $PM upgrade $YCM_DEPS
+fi
 
 # install vim plugins
 vim +PlugInstall +qall 
 
-# install YMC vim plugin if not already installed
+# YCM full install
 if $YCM_INSTALL || $DO_ALL; then
-	yes | $PM install $YCM_DEPS
-	yes | $PM upgrade $YCM_DEPS
 	cd "$YCM_PATH"
 	git submodule update --init --recursive
 	python3 install.py --all
